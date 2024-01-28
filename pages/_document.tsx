4666ba76e0b652ecc14bc13272336ed79c2c4c45
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2024-01-28 19:47:10 +0100

    html locale remove

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,8 @@
-import { SupportedLangs } from '@/src/types';
-import useTranslation from 'next-translate/useTranslation';
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  const { lang } = useTranslation();
-  const currentLang = lang as SupportedLangs;
-
   return (
-    <Html lang={currentLang}>
+    <Html>
       <Head />
       <body>
         <Main />
