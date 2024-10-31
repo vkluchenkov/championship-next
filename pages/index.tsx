11d6af4ad91b81533810ac194becb9134330861e
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2024-10-31 13:55:57 +0100

    refactor: home page welcome text

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,10 +37,56 @@ export default function Home() {
   const welcomeText = (
     <Trans
       i18nKey='home:welcomeText'
-      components={[
-        <p className={styles.content__text} key={0} />,
-        <span className={styles.content__text_accent} key={1} />,
-      ]}
+      components={[<span className={styles.content__text_accent} key={0} />]}
+    />
+  );
+
+  const teachersText = (
+    <Trans
+      i18nKey='home:teachers'
+      components={[<span className={styles.content__text_accent} key={0} />]}
+    />
+  );
+
+  const bullet1 = (
+    <Trans
+      i18nKey='home:bullet1'
+      components={[<span className={styles.content__text_accent} key={0} />]}
+    />
+  );
+
+  const bullet2 = (
+    <Trans
+      i18nKey='home:bullet2'
+      components={[<span className={styles.content__text_accent} key={0} />]}
+    />
+  );
+
+  const bullet3 = (
+    <Trans
+      i18nKey='home:bullet3'
+      components={[<span className={styles.content__text_accent} key={0} />]}
+    />
+  );
+
+  const bullet4 = (
+    <Trans
+      i18nKey='home:bullet4'
+      components={[<span className={styles.content__text_accent} key={0} />]}
+    />
+  );
+
+  const bullet5 = (
+    <Trans
+      i18nKey='home:bullet5'
+      components={[<span className={styles.content__text_accent} key={0} />]}
+    />
+  );
+
+  const bullet6 = (
+    <Trans
+      i18nKey='home:bullet6'
+      components={[<span className={styles.content__text_accent} key={0} />]}
     />
   );
 
@@ -53,7 +99,16 @@ export default function Home() {
           <div className={styles.teachersContainer}>
             <Image src={socialPoster} alt={t('home:coverImageAlt')} fill priority />
           </div>
-          {welcomeText}
+          <p className={styles.content__text}>{welcomeText}</p>
+          <ul className={styles.content__list}>
+            <li>{bullet1}</li>
+            <li>{bullet2}</li>
+            <li>{bullet3}</li>
+            <li>{bullet4}</li>
+            <li>{bullet5}</li>
+            <li>{bullet6}</li>
+          </ul>
+          <p className={styles.content__text}>{teachersText}</p>
         </section>
         <section className={styles.schedule}>
           <h2 className={clsx(textStyles.h2, textStyles.uppercase, textStyles.glow)}>
